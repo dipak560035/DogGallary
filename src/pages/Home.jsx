d43d10ffddbc6f6fdde0b22ee1e7ca16bf7adbc3
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-04-15 17:04:24 +0545

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,64 @@
+
+// import { useEffect, useState } from "react";
+// import { getRandomDogImage } from "../api/api";
+
+// export default function Home() {
+//   const [image, setImage] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchImage() {
+//       const img = await getRandomDogImage();
+//       setImage(img);
+//     }
+//     fetchImage();
+//   }, []);
+
+//   return (
+//     <div className="flex flex-col items-center">
+//       <h1 className="text-3xl font-bold mb-8 text-center">🐶 Random Dog</h1>
+//       {image ? (
+//         <img
+//           src={image}
+//           alt="Random Dog"
+//           className="w-96 h-96 object-cover rounded-lg shadow-lg"
+//         />
+//       ) : (
+//         <p>Loading...</p>
+//       )}
+//     </div>
+//   ); 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { useEffect, useState } from "react";
 import { getRandomDogImage } from "../api/api";
 
